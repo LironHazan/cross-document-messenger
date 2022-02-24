36ae2d6c73358f110a8215866b8fb4c8fb25cd0c
lironh <lironenk@gmail.com>
2022-02-24 21:03:04 +0200

    feat(*): fixed build

---
+++ scripts/build.js
@@ -0,0 +1,14 @@
+require('esbuild')
+  .build({
+    entryPoints: ['lib/index.ts'],
+    bundle: true,
+    sourcemap: true,
+    minify: true,
+    loader: { '.ts': 'ts' },
+    format: 'esm',
+    target: ['esnext'],
+    outdir: 'dist/cross-document-messenger',
+    splitting: true,
+  })
+  .then(() => console.log('⚡ Done'))
+  .catch(() => process.exit(1));
